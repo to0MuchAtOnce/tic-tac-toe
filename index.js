499646bbf2da7cfea99bb216f8572fc7e75e1ef1
Tom lewis <tom.lewis488@gmail.com>
2024-08-01 22:40:16 +0200

    Add gameboard function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Store the gameboard as an array inside of a Gameboard object
+// Factory function wrapped in an IIFE containing rows, columns, and the gameboard array.
+const gameboard = (function () {
+  const board = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+  ];
+  return { board };
+})();
+
+console.log(gameboard.board);
